646ceb5debf134053ed6aea22336c5e41c5e446e
LaunchGemini <metabaseguy@proton.me>
2022-02-14 10:11:39 +0000

    fixed import

--- app/src/androidTest/java/com/dorvis/androidtensorflowlite/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/dorvis/androidtensorflowlite/ExampleInstrumentedTest.java
@@ -1,4 +1,8 @@
 package com.dorvis.androidtensorflowlite;
 
 import android.content.Context;
-i
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+imp
