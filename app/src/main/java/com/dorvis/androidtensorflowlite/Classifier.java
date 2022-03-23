2aeed5f97b32d9e01e6a9220d2b4f6de1a5ac118
LaunchGemini <metabaseguy@proton.me>
2022-03-23 19:49:37 +0000

    font weight

--- app/src/main/java/com/dorvis/androidtensorflowlite/Classifier.java
+++ app/src/main/java/com/dorvis/androidtensorflowlite/Classifier.java
@@ -7,4 +7,11 @@ public interface Classifier {
 
     class Recognition {
         /**
-         * A unique identifier for what has been recognized. Specific to the class, not the inst
+         * A unique identifier for what has been recognized. Specific to the class, not the instance of
+         * the object.
+         */
+        private final String id;
+
+        /**
+         * Display name for the recognition.
+   
