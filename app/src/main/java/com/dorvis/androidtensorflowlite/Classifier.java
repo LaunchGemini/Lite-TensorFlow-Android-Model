68ba2f29dbbda649688f8680a369fd6902afb2a5
LaunchGemini <metabaseguy@proton.me>
2022-03-23 18:08:25 +0000

    Avoid compiler warnings.

--- app/src/main/java/com/dorvis/androidtensorflowlite/Classifier.java
+++ app/src/main/java/com/dorvis/androidtensorflowlite/Classifier.java
@@ -5,4 +5,6 @@ import android.graphics.Bitmap;
 import java.util.List;
 public interface Classifier {
 
- 
+    class Recognition {
+        /**
+         * A unique identifier for what has been recognized. Specific to the class, not the inst
