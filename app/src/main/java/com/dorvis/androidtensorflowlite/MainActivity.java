31c6faffabcbb2ba6d419cd3eee3053e2018496f
LaunchGemini <metabaseguy@proton.me>
2022-04-02 16:19:04 +0000

    Bumping version to 1.2.0

--- app/src/main/java/com/dorvis/androidtensorflowlite/MainActivity.java
+++ app/src/main/java/com/dorvis/androidtensorflowlite/MainActivity.java
@@ -1,3 +1,7 @@
 package com.dorvis.androidtensorflowlite;
 
-import android.support
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.graphics.Bitmap;
+import android.text.method.ScrollingMovementMe
