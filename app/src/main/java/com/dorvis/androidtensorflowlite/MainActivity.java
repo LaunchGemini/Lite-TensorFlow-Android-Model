238d3f4674adff2d3851150f70eff65ff2e19ab4
LaunchGemini <metabaseguy@proton.me>
2022-04-02 16:51:10 +0000

    Remove obsolete class

--- app/src/main/java/com/dorvis/androidtensorflowlite/MainActivity.java
+++ app/src/main/java/com/dorvis/androidtensorflowlite/MainActivity.java
@@ -4,4 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.graphics.Bitmap;
-import android.text.method.ScrollingMovementMe
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+impo
