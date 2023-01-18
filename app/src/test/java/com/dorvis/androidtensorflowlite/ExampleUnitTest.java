d494ba18483803e63a8a2cba4727df7a157557a5
LaunchGemini <metabaseguy@proton.me>
2023-01-18 18:50:18 +0000

    Bump minimist from 1.2.0 to 1.2.5

--- app/src/test/java/com/dorvis/androidtensorflowlite/ExampleUnitTest.java
+++ app/src/test/java/com/dorvis/androidtensorflowlite/ExampleUnitTest.java
@@ -1,3 +1,10 @@
 package com.dorvis.androidtensorflowlite;
 
-import org.j
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @s
