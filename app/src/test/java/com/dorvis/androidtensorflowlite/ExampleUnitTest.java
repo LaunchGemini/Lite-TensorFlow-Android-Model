789ea2b322262dfbdb5577d81c610ce74541d227
LaunchGemini <metabaseguy@proton.me>
2023-01-18 22:08:45 +0000

    missing class

--- app/src/test/java/com/dorvis/androidtensorflowlite/ExampleUnitTest.java
+++ app/src/test/java/com/dorvis/androidtensorflowlite/ExampleUnitTest.java
@@ -7,4 +7,7 @@ import static org.junit.Assert.*;
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
- * @s
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Tes
